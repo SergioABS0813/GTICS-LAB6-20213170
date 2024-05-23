400dfd4b067f923c0d9e8e2db94c3f8d9428c59f
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-05-22 19:14:10 -0500

    primer commit IntelliJ

---
+++ lab6-gtics/src/main/java/com/example/lab6gtics/Lab6GticsApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab6gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab6GticsApplication.class, args);
+    }
+
+}
---
+++ lab6-gtics/src/test/java/com/example/lab6gtics/Lab6GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab6gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
